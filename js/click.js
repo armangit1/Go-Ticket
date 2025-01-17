88de5dd999f18bcdcbb8430444b35d84f1978eaa
Midland Computer <aharmankhan333@gmail.com>
2025-01-17 19:38:26 +0600

    complit  ticket section

---
+++ js/click.js
@@ -0,0 +1,319 @@
+
+getId('A1').addEventListener('click', function () {
+   
+    btnclick('A1')
+      
+      chackArray();
+      
+    });
+  
+  getId('A2').addEventListener('click', function () {
+     
+    btnclick('A2')
+      
+      chackArray();
+      
+    });
+  
+   
+  
+  getId('A3').addEventListener('click', function () {
+     
+    btnclick('A3')
+      
+      chackArray();
+      
+    });
+  
+  getId('A4').addEventListener('click', function () {
+     
+    btnclick('A4')
+      
+      chackArray();
+      
+    });
+  
+  getId('B1').addEventListener('click', function () {
+     
+    btnclick('B1')
+      
+      chackArray();
+      
+    });
+
+  getId('B2').addEventListener('click', function () {
+     
+    btnclick('B2')
+      
+      chackArray();
+      
+    });
+  getId('B3').addEventListener('click', function () {
+     
+    btnclick('B3')
+      
+      chackArray();
+      
+    });
+  getId('B4').addEventListener('click', function () {
+     
+    btnclick('B4')
+      
+      chackArray();
+      
+    });
+  getId('C1').addEventListener('click', function () {
+     
+    btnclick('C1')
+      
+      chackArray();
+      
+    });
+  getId('C2').addEventListener('click', function () {
+     
+    btnclick('C4')
+      
+      chackArray();
+      
+    });
+  getId('D1').addEventListener('click', function () {
+     
+    btnclick('D1')
+      
+      chackArray();
+      
+    });
+  getId('D2').addEventListener('click', function () {
+     
+    btnclick('D2')
+      
+      chackArray();
+      
+    });
+  getId('D3').addEventListener('click', function () {
+     
+    btnclick('D3')
+      
+      chackArray();
+      
+    });
+  getId('D4').addEventListener('click', function () {
+     
+    btnclick('D4')
+      
+      chackArray();
+      
+    });
+  getId('E1').addEventListener('click', function () {
+     
+    btnclick('E1')
+      
+      chackArray();
+      
+    });
+  getId('E2').addEventListener('click', function () {
+     
+    btnclick('E2')
+      
+      chackArray();
+      
+    });
+  getId('E3').addEventListener('click', function () {
+     
+    btnclick('E3')
+      
+      chackArray();
+      
+    });
+  getId('E4').addEventListener('click', function () {
+     
+    btnclick('E4')
+      
+      chackArray();
+      
+    });
+  getId('F1').addEventListener('click', function () {
+     
+    btnclick('F1')
+      
+      chackArray();
+      
+    });
+  getId('F2').addEventListener('click', function () {
+     
+    btnclick('F2')
+      
+      chackArray();
+      
+    });
+  getId('F3').addEventListener('click', function () {
+     
+    btnclick('F3')
+      
+      chackArray();
+      
+    });
+  getId('F4').addEventListener('click', function () {
+     
+    btnclick('F4')
+      
+      chackArray();
+      
+    });
+  getId('G1').addEventListener('click', function () {
+     
+    btnclick('G1')
+      
+      chackArray();
+      
+    });
+  getId('G2').addEventListener('click', function () {
+     
+    btnclick('G2')
+      
+      chackArray();
+      
+    });
+  getId('G3').addEventListener('click', function () {
+     
+    btnclick('G3')
+      
+      chackArray();
+      
+    });
+  getId('G4').addEventListener('click', function () {
+     
+    btnclick('G4')
+      
+      chackArray();
+      
+    });
+  getId('H1').addEventListener('click', function () {
+     
+    btnclick('H1')
+      
+      chackArray();
+      
+    });
+  getId('H2').addEventListener('click', function () {
+     
+    btnclick('H2')
+      
+      chackArray();
+      
+    });
+  getId('H3').addEventListener('click', function () {
+     
+    btnclick('H3')
+      
+      chackArray();
+      
+    });
+  getId('H4').addEventListener('click', function () {
+     
+    btnclick('H4')
+      
+      chackArray();
+      
+    });
+  getId('I1').addEventListener('click', function () {
+     
+    btnclick('I1')
+      
+      chackArray();
+      
+    });
+  getId('I2').addEventListener('click', function () {
+     
+    btnclick('I2')
+      
+      chackArray();
+      
+    });
+  getId('I3').addEventListener('click', function () {
+     
+    btnclick('I3')
+      
+      chackArray();
+      
+    });
+  getId('I4').addEventListener('click', function () {
+     
+    btnclick('I4')
+      
+      chackArray();
+      
+    });
+  getId('J1').addEventListener('click', function () {
+     
+    btnclick('J1')
+      
+      chackArray();
+      
+    });
+  getId('J2').addEventListener('click', function () {
+     
+    btnclick('J2')
+      
+      chackArray();
+      
+    });
+  getId('J3').addEventListener('click', function () {
+     
+    btnclick('J3')
+      
+      chackArray();
+      
+    });
+  getId('J4').addEventListener('click', function () {
+     
+    btnclick('J4')
+      
+      chackArray();
+      
+    });
+
+
+
+
+    function bookingsit(){
+      if (setArray.length >= 1) {
+
+        showEliment('set1div')
+  
+      }
+      if(setArray.length == 0) {
+        hiddenEliment('set1div')
+        
+      }
+       if (setArray.length >= 2) {
+        showEliment('set2div')
+  
+      }
+       if (setArray.length == 1) {
+        hiddenEliment('set2div')
+  
+      }
+     
+       if (setArray.length >= 3) {
+        showEliment('set3div')
+  
+      }
+       if (setArray.length == 2) {
+        hiddenEliment('set3div')
+  
+      }
+       if (setArray.length >= 4) {
+        showEliment('set4div')
+  
+      }
+       if (setArray.length == 3) {
+        hiddenEliment('set4div')
+  
+      }
+  
+      getId('set1').innerText  = setArray[0];
+      getId('set2').innerText  = setArray[1];
+      getId('set3').innerText  = setArray[2];
+      getId('set4').innerText  = setArray[3];
+    }
---
+++ js/utility.js
@@ -0,0 +1,154 @@
+function copy(text) {
+  navigator.clipboard.writeText(text)
+
+}
+
+const copy15 = document.getElementById('copy-15');
+copy15.addEventListener('click', function () {
+  copy('NEW15')
+  copy15.innerText = 'Copy Coupon';
+
+
+
+});
+
+
+
+const copy20 = document.getElementById('copy-20');
+copy20.addEventListener('click', function () {
+  copy('Couple 20')
+  copy20.innerText = 'Copy Coupon'
+  console.log('copy')
+})
+
+
+// ============================================================
+
+document.getElementById('Apply').addEventListener('click', function () {
+  const Applyin = document.getElementById('Apply-input');
+  const Applyintext = Applyin.value;
+
+  if (setArray.length == 0) {
+    document.getElementById('Apply').innerText = 'Feild'
+    getId('gtotal').innerText = '---';
+    Applyin.value = '';
+    return
+  }
+
+  if (Applyintext == 'NEW15') {
+    document.getElementById('Apply').innerText = 'Applyed 15%'
+
+    const miampraice = getId('total').innerText;
+    const pricenum = parseInt(miampraice);
+    const grantedprice = pricenum - ((pricenum * 15) / 100);
+    getId('gtotal').innerText = grantedprice;
+
+  }
+  else if (Applyintext == 'Couple 20') {
+    document.getElementById('Apply').innerText = 'Applyed 20%'
+    const miampraice = getId('total').innerText;
+    const pricenum = parseInt(miampraice);
+    const grantedprice = pricenum - ((pricenum * 20) / 100);
+    getId('gtotal').innerText = grantedprice;
+
+  }
+  else {
+
+    document.getElementById('Apply').innerText = 'Feild'
+    getId('gtotal').innerText = '---';
+  }
+
+
+})
+
+
+// =========================================================================
+
+const setArray = [];
+const allId = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4','E1' ,'E2' ,'E3' ,'E4' ,'F1' ,'F2' ,'F3' ,'F4', 'G1', 'G2', 'G3', 'G4', 'H1', 'H2', 'H3', 'H4', 'I1', 'I2', 'I3', 'I4', 'J1', 'J2', 'J3', 'J4']
+
+for (const st of allId) {
+  getId(st).style.cursor = 'pointer';
+}
+
+function chackArray() {
+  console.log(setArray)
+
+  for (const item of allId) {
+    getId(item).style.backgroundColor = '#f4f7f7';
+
+    bookingsit();
+  
+  }
+
+  getId('Apply').innerText = 'Apply'
+
+
+  chackBg();
+
+  getId('count').innerText = setArray.length;
+  // getId('total').innerText = setArray.length*550;
+  let count = setArray.length;
+  getId('total').innerText = count * 550;
+
+  getId('gtotal').innerText = '---';
+
+}
+
+
+function getId(setId) {
+
+  const setIdtext = document.getElementById(setId);
+  return setIdtext;
+
+}
+
+
+function chackBg() {
+  // Loop through all items in the array
+  for (const item of setArray) {
+    changebg(item); // Call the 'changebg' function for each item
+  }
+}
+
+function changebg(bgid) {
+  const setbg = document.getElementById(bgid); // Get the element by its ID
+  if (setbg) { // Ensure the element exists
+    setbg.style.backgroundColor = '#1cd100d3'; // Change its background color
+  } else {
+    console.error(`Element with ID "${bgid}" not found.`);
+  }
+}
+
+
+
+
+function btnclick(inid) {
+  if (!setArray.includes(inid) && setArray.length < 4) {
+    setArray.push(inid);
+  }
+  else {
+    const index = setArray.indexOf(inid); // Find the index of 'A2'
+    if (index > -1) {
+      setArray.splice(index, 1); // Remove 'A2' from the array
+    }
+  }
+}
+
+
+function hiddenEliment(elementId) {
+  const elementid = document.getElementById(elementId);
+  elementid.classList.add('hidden')
+
+
+
+}
+function showEliment(elementId) {
+  const elementid = document.getElementById(elementId);
+  elementid.classList.remove('hidden')
+  elementid.classList.add('flex')
+
+
+}
+
+
